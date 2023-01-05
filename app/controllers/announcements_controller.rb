6bc388cd372c64f3f133692d1b669e557f4b1a24
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2023-01-05 12:38:55 +0300

    define the create controller

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,7 +4,30 @@ class AnnouncementsController < ApplicationController
     end
 
     def show
-        announcement = Announcement.find(params[:id])
+        announcement = find_announcement
         render json: announcement
+    end
+
+    def create
+        announcement = Announcement.create!(announcement_params)
+        render json: announcement
+    end
+
+    def update 
+        a
+    end
+
+    def destroy 
+
+    end
+
+    private
+
+    def find_announcement
+        Announcement.find(params[:id])
+    end
+
+    def announcement_params
+        params.permit(:title, :content)
     end
 end
